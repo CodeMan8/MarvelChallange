e37aee7a158a0c877ce44f898bebcc283f32bc89
bartu <bartuakman8@gmail.com>
2020-09-17 16:11:14 +0300

    Initial Commit

---
+++ MarvelChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelChallenge
+//
+//  Created by Bartu akman on 17.09.2020.
+//  Copyright © 2020 Bartu akman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

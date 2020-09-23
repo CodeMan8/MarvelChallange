65f6007c54d0bbbaba3363b411b2ae12cececeed
bartu <bartuakman8@gmail.com>
2020-09-23 21:19:45 +0300

    model class has been changed as struct

--- MarvelChallenge/Model/Character.swift
+++ MarvelChallenge/Model/Character.swift
@@ -7,41 +7,23 @@
 //
 
 import UIKit
-class Character {
-    private var _id: Int!
-    private var _name: String!
-    private var _imageUrl: String!
-    private var _description: String?
-    private var _comics: [String?]
+struct Character {
+     var id: Int!
+     var name: String!
+     var imageUrl: String!
+     var description: String?
+     var comics: [String?]
     
-    
-    var id: Int {
-        return _id
-    }
-    var name: String {
-        return _name
-    }
-    
-    var imageUrl: String {
-        return _imageUrl
-    }
-    
-    
-    var comics: [String?] {
-        return _comics
-    }
-    var description: String? {
-        return _description
-    }
+ 
     
     
     
     init(id: Int,name: String, imageUrl: String, description: String?, comics: [String?]) {
-        _id = id
-        _name = name
-        _imageUrl = imageUrl
-        _description = description
-        _comics = comics
+        self.id = id
+        self.name = name
+        self.imageUrl = imageUrl
+        self.description = description
+        self.comics = comics
     }
     
     
